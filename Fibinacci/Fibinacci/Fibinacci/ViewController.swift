0cfd1bd36b05c3fdc095844d2937494f3b27b747
douggibson <douggibsoncomposer@me.com>
2014-09-30 14:32:00 -0400

    first model

--- Fibinacci/Fibinacci/Fibinacci/ViewController.swift
+++ Fibinacci/Fibinacci/Fibinacci/ViewController.swift
@@ -6,15 +6,24 @@
 //  Copyright (c) 2014 Macbook. All rights reserved.
 //
 
-import UIKit
-
 class ViewController: UIViewController {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+           var fibo = FibonacciModel ()
+           var returnedArray: [Int] = []
+           returnedArray = fibo.calculateFibonacciNumbers(minimum2: 5)
+        
+           println()
+           for number in returnedArray {
+            prinnt("\(number),")
+        }
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -22,4 +31,17 @@ class ViewController: UIViewController {
 
 
 }
+
+func addASlider(){
+    
+    var theSlider = UISlider(frame: CGRectMake(60, 244, 200, 20))
+    self.view.addSubview(theSlider)
+    self.addASlider()
+}
+
+
+    
+    
+
+
 
